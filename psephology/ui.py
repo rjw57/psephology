6b72732f0c8f9fea4fec61abd721eacc9eb7bb37
Rich Wareham <rjw57@cam.ac.uk>
2017-08-22 18:00:45 +0100

    show results in UI

--- psephology/ui.py
+++ psephology/ui.py
@@ -1,11 +1,23 @@
 from flask import (
     Blueprint, current_app, render_template,
 )
+from sqlalchemy import func
+from sqlalchemy.orm import joinedload
 
-from .model import db
+from .model import db, Voting, Constituency
 
 blueprint = Blueprint('ui', __name__, template_folder='templates/ui')
 
 @blueprint.route('/')
 def index():
-    return render_template('index.html')
+    results = (
+        db.session.query(Voting, func.max(Voting.count), func.sum(Voting.count))
+        .select_from(Voting)
+        .join(Constituency)
+        .group_by(Constituency).order_by(Constituency.name)
+        .options(
+            joinedload(Voting.constituency),
+            joinedload(Voting.party)
+        )
+    )
+    return render_template('index.html', results=results)
